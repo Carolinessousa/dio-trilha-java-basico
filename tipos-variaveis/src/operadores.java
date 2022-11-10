65058b4e7101e7f0d67be4a55920e574e5350659
Caroline <caroline.sousa24@otlook.com>
2022-11-10 17:06:10 -0300

    operadores

---
+++ tipos-variaveis/src/operadores.java
@@ -0,0 +1,11 @@
+public class operadores {
+    
+    public static void main(String[] args) {
+
+    String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+    System.out.print(nomeCompleto);
+    }
+
+}
+
